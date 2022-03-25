d1a4d8f923812e9010b00c3d293211f4be86da73
nvpost <nvbpost@yandex.ru>
2022-03-25 16:49:01 +0300

    'cm'

--- sql/send_data_sql.py
+++ sql/send_data_sql.py
@@ -1,15 +1,28 @@
-import config
+import pymysql
 
-db = config.db
+
+db = pymysql.connect(host="localhost",
+                      user='root',
+                      password='',
+                      database='news_site',
+                      charset='utf8mb4')
+
+# db = config.db
 
 cursor = db.cursor()
 
 def add_data(newsArr):
 
-    query = """INSERT INTO products(site_id, group_id, lang, news_date, title, link, status)
+    query = """INSERT INTO news(site_id, group_id, lang, news_date, title, link, status)
     values(%s, %s, %s, %s, %s, %s, %s)"""
 
-
-    cursor = db.cursor()
     cursor.executemany(query,newsArr)
+    db.commit()
+
+def add_donor(donorArr):
+    query = """INSERT INTO news_donor(site_id, name, link, category, lang)
+    values(%s, %s, %s, %s, %s)"""
+
+
+    cursor.execute(query,donorArr)
     db.commit()
